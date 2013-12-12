4d88bda36ce040f90a73d3d3eb370209e739c6a8
Scott Beslow <scottbeslow@Scotts-MacBook-Pro.local>
2013-12-11 18:17:41 -0600

    Modifications to the parser.  Broke it for now

--- src/com/hydrophilik/cso/parseHtml/MwrdCsoSynopsisParser.java
+++ src/com/hydrophilik/cso/parseHtml/MwrdCsoSynopsisParser.java
@@ -1,14 +1,69 @@
 package com.hydrophilik.cso.parseHtml;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public abstract class MwrdCsoSynopsisParser {
 
 	public static Map<String, CsoEvent> buildEvents(Document doc) {
+
+		Map<String, CsoEvent> events = new HashMap<String, CsoEvent>();
 		
+		// Get the starting date
+		String date = getUValue(doc.getElementById("lbdate"));	
+		if (null == date)
+			return null;
 		
-		return null;
+		// Some of the 
+		
+
+		Element eventTable = doc.getElementById("DG1");
+		if (null == eventTable)
+			return null;
+
+		Elements eventsTags = eventTable.getElementsByTag("tr");
+		
+		for (Element event : eventsTags) {
+			Elements theRow = event.getElementsByTag("td");
+			
+			int index = 0;
+			String [] aTable = new String[5];
+			
+			for (Element field : theRow) {
+				aTable[index] = field.text();
+				index++;
+			}
+			
+			if (false == aTable[0].equals("Outfall Location")) {
+				CsoEvent csoEvent = new CsoEvent(date);
+				csoEvent.buildCsoEvent(aTable);
+				
+				if (csoEvent.isValid()) {
+					// events.put("",csoEvent);
+				}
+				
+			}
+		}
+		
+		return events;
+
+	}
+	
+	private static String getUValue(Element content) {
+		
+		if (null == content)
+			return null;
+		
+		Elements links = content.getElementsByTag("u");
+		String retVal = null;
+		if (null != links) {
+			retVal = links.first().text();
+		}
+		
+		return retVal;
 	}
 }
